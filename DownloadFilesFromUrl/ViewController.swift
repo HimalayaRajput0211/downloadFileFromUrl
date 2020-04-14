81075650335dbd7e8db7834bb95f0213e970e512
HimalayaRajput0211 <himalaya@vinsol.com>
2020-04-14 18:32:00 +0530

    added quickLook functionality partially

--- DownloadFilesFromUrl/ViewController.swift
+++ DownloadFilesFromUrl/ViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import Alamofire
+import QuickLook
 
 class ViewController: UIViewController {
     
     private var filePaths = [String]()
+    private var fileURLs = [NSURL]()
+    let quickLookController = QLPreviewController()
     @IBOutlet private weak var urlTextField: UITextField!
     @IBOutlet private weak var downloadFileButton: UIButton!
     @IBOutlet private weak var progressBar: CircularProgressView!
@@ -21,7 +24,13 @@ class ViewController: UIViewController {
         if let paths = UserDefaults.standard.object(forKey: PersistenceKey.savedFileUrls) as? [String] {
             filePaths = paths
         }
+        quickLookController.dataSource = self
+        convertPathToURL()
     }
+    @IBAction func openQuickLookController(_ sender: UIButton) {
+        present(quickLookController, animated: true, completion: nil)
+    }
+    
     
     @IBAction func downloadFile(_ sender: UIButton) {
         guard let urlString = urlTextField.text else { return }
@@ -32,12 +41,14 @@ class ViewController: UIViewController {
     
     private func downloadFileFromString(_ urlString: String) {
         guard let url = URL(string: urlString) else { return }
-//        AF.download(url).response { (response) in
-        AF.download(url, interceptor: nil, to: .none).response { response in
+        let destination = DownloadRequest.suggestedDownloadDestination(for: .cachesDirectory, in: .userDomainMask, options: .createIntermediateDirectories)
+        AF.download(url, to: destination).response { response in
             switch response.result {
             case .success(let fileUrl):
                 guard let filePath = fileUrl?.path else { return }
                 self.filePaths.append(filePath)
+                let url = NSURL(fileURLWithPath: filePath, relativeTo: fileUrl)
+                self.fileURLs.append(url)
                 UserDefaults.standard.set(self.filePaths, forKey: PersistenceKey.savedFileUrls)
             case .failure(let error): print(error)
             }
@@ -48,7 +59,28 @@ class ViewController: UIViewController {
     }
     
 }
+extension ViewController: QLPreviewControllerDataSource {
+    
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        return fileURLs.count
+    }
+    
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        return fileURLs[index]
+    }
+    
+    private func convertPathToURL() {
+        for path in filePaths {
+            let fileurl = NSURL(fileURLWithPath: path)
+            fileURLs.append(fileurl)
+        }
+    }
+    
+}
 
 struct PersistenceKey {
     static let savedFileUrls = "saved_file_urls"
 }
+
+
+
